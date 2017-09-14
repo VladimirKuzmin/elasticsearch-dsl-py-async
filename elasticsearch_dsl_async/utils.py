798f441f47c0d2a045b60adf2430613c7cde99fc
Vladimir Kuzmin <svarog.online@gmail.com>
2017-09-14 10:32:01 +0300

    Update utils.py

--- elasticsearch_dsl_async/utils.py
+++ elasticsearch_dsl_async/utils.py
@@ -2,6 +2,6 @@ import asyncio
 
 
 def execute_queries(*queries):
-    future = asyncio.gather(*(q.execute_async() for q in queries))
+    future = asyncio.gather(*(q.execute_future() for q in queries))
     loop = future._loop
     return loop.run_until_complete(future)
