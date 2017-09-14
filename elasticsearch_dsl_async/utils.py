55a195adeea62c3a4d82fcb70d33899ba7745084
Vladimir Kuzmin <Vladimir.A.Kuzmin@gmail.com>
2017-09-14 08:31:59 +0300

    use event loop from gathering future

--- elasticsearch_dsl_async/utils.py
+++ elasticsearch_dsl_async/utils.py
@@ -1,6 +1,7 @@
 import asyncio
 
 
-def execute_queries(*queries, loop=None):
-    loop = loop or asyncio.get_event_loop()
-    return loop.run_until_complete(asyncio.gather(*(q.execute_async() for q in queries)))
+def execute_queries(*queries):
+    future = asyncio.gather(*(q.execute_async() for q in queries))
+    loop = future._loop
+    return loop.run_until_complete(future)
